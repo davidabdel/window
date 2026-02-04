8a65009f0f4f871ffeb5154c7055284f2836a1a3
UConnect <info@uconnect.com.au>
2026-02-05 08:31:35 +1100

    Allow Admin Header in CORS

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export default {
         const corsHeaders = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Headers': 'Content-Type, X-Admin-Key',
         };
 
         if (request.method === 'OPTIONS') {
